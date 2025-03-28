4c934dca7361a6609b609cff005e932e4c148bea
WTL04 <williamluu04@gmail.com>
2025-03-27 21:38:18 -0700

    saving progress

---
+++ backend/app/api/notes.md
@@ -0,0 +1,7 @@
+# API 
+
+This is the directory where API endpoints will be.
+ex:
+- Running predictions 
+- Starting model training
+
---
+++ backend/app/models/emotion_model.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn 
+
+
+class Model(nn.Module):
+    
+    # initialize model layers
+    def __init__(self):
+
