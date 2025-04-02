55fd9d09ac007fd491a682b0a26d8f8f69a88c48
WTL04 <williamluu04@gmail.com>
2025-04-01 23:14:54 -0700

    added face detection using opencv haarcascades

--- backend/app/models/face_detector.py
+++ backend/app/models/face_detector.py
@@ -1,17 +1,30 @@
 import cv2
-import mediapipe as mp # pretrained face detection model 
+# loading pre-trained classifer
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 # open default webcam
 cam = cv2.VideoCapture(0)
-if not cap.isOpened():
+if not cam.isOpened():
     raise IOError("Cannot open webcam")
 
 # loop capturing webcam frames
 while True:
+
+    # ret: bool indicating if frame was captured successfully
+    # frame: the actual image frame being captured
     ret, frame = cam.read()
+    if not ret:
+        break
     
-    # display captured frame
-    cv2.imshow("Camera", frame)
+    # convert frame to grayscale since Haar Cascades works better with grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))
+
+     # Draw bounding boxes around faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    cv2.imshow("YOLOv8 Tracking", frame)
 
     # press 'q' to exit loop 
     if cv2.waitKey(1) == ord('q'):
@@ -19,4 +32,4 @@ while True:
 
 # release capture and destory all windows
 cam.release()
-cv2.destoryAllWindows()
+cv2.destroyAllWindows()
